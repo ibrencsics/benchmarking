6ff2c4c71b12e83014ca9867d6dddf04eaf8eb91
ivan <ivan.brencsics@gmail.com>
2017-12-13 23:19:40 +0100

    Ribbon experiments

--- ribbon-eval/src/main/java/org/ib/eval/ribbon/RibbonClient.java
+++ ribbon-eval/src/main/java/org/ib/eval/ribbon/RibbonClient.java
@@ -1,12 +1,19 @@
 package org.ib.eval.ribbon;
 
+import com.netflix.client.config.DefaultClientConfigImpl;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.client.config.IClientConfigKey;
+import com.netflix.config.ConfigurationManager;
+import com.netflix.loadbalancer.ILoadBalancer;
 import io.netty.buffer.ByteBuf;
 import io.reactivex.netty.protocol.http.client.HttpClientRequest;
 import io.reactivex.netty.protocol.http.client.HttpClientResponse;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+import org.apache.commons.configuration.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import rx.Observer;
@@ -22,7 +29,7 @@ import com.netflix.loadbalancer.Server;
 public class RibbonClient {
 
     private static Logger logger = LogManager.getLogger(RibbonClient.class);
-    private static final int REQ_COUNT = 50;
+    private static final int REQ_COUNT = 10;
 
 
     public static void main(String[] args) throws Exception {
@@ -30,8 +37,23 @@ public class RibbonClient {
     }
 
     private static void call() throws Exception {
+        ConfigurationManager.loadPropertiesFromResources("sample-client.properties");  // 1
+        System.out.println(ConfigurationManager.getConfigInstance().getString("sample-client.ribbon.listOfServers"));
+
+//        Configuration config = ConfigurationManager.getConfigInstance();
+//        config.setProperty("client1.niws.client." + IClientConfigKey.Keys.DeploymentContextBasedVipAddresses, "dummy:7001");
+//        config.setProperty("client1.niws.client." + IClientConfigKey.Keys.InitializeNFLoadBalancer, "true");
+//        config.setProperty("client1.niws.client." + IClientConfigKey.Keys.NFLoadBalancerClassName, DynamicServerListLoadBalancer.class.getName());
+//        config.setProperty("client1.niws.client." + IClientConfigKey.Keys.NFLoadBalancerRuleClassName, RoundRobinRule.class.getName());
+//        config.setProperty("client1.niws.client." + IClientConfigKey.Keys.NIWSServerListClassName, DiscoveryEnabledNIWSServerList.class.getName());
+//        config.setProperty("client1.niws.client." + IClientConfigKey.Keys.NIWSServerListFilterClassName, ZoneAffinityServerListFilter.class.getName());
+        IClientConfig clientConfig = IClientConfig.Builder.newBuilder(DefaultClientConfigImpl.class, "sample-client").build();
+        System.out.println(clientConfig.getPropertyAsString(IClientConfigKey.Keys.MaxAutoRetries, "def"));
+
         List<Server> servers = Lists.newArrayList(new Server("localhost:8000"), new Server("localhost:8001"));
         BaseLoadBalancer lb = LoadBalancerBuilder.newBuilder()
+            .withClientConfig(clientConfig)
+//            .buildLoadBalancerFromConfigWithReflection();
             .buildFixedServerListLoadBalancer(servers);
 
         LoadBalancingHttpClient<ByteBuf, ByteBuf> client = RibbonTransport.newHttpClient(lb);
@@ -50,16 +72,17 @@ public class RibbonClient {
             @Override
             public void onNext(HttpClientResponse<ByteBuf> args) {
                 latch.countDown();
-                logger.debug("Got response: " + args.getStatus());
+                args.getContent().subscribe(d -> logger.debug(d.toString(StandardCharsets.UTF_8)));
             }
         };
 
         for (int i = 0; i < REQ_COUNT ; i++) {
+            Thread.sleep(500);
             HttpClientRequest<ByteBuf> request = HttpClientRequest.createGet("/" + i);
             client.submit(request).subscribe(observer);
         }
 
         latch.await();
-        System.out.println(lb.getLoadBalancerStats());
+        logger.debug(lb.getLoadBalancerStats());
     }
 }
